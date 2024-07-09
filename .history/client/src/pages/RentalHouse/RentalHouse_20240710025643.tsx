ff625801d476a0786dfbd8e0c362d98701261971
Nguyen-Duc-Trong <trongnguyen160902@gmail.com>
2024-07-10 04:19:36 +0700

    up to pull

---
+++ .history/client/src/pages/RentalHouse/RentalHouse_20240710025643.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostingList from '../HomePage/components/PostingList.tsx'
+
+const RentalHouse = () => {
+  return (
+    <div className='mt-[30px]'>
+      <h2 className='text-center text-[32px] font-bold mb-[20px]'>Nhà Cho Thuê</h2>
+      <PostingList/>
+    </div>
+  )
+}
+
+export default RentalHouse
--- .history/client/src/pages/RentalHouse/RentalHouse_20240710030017.tsx
+++ .history/client/src/pages/RentalHouse/RentalHouse_20240710030017.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import PostingList from '../HomePage/components/PostingList.tsx'
 
-const RentingRoom = () => {
+const RentalHouse = () => {
   return (
-    <div className='mt-[10px]'>
-      <h2 className='text-center text-[32px] font-bold mb-[5px]'>Cho Thuê Phòng Trọ</h2>
-      <p className='text-[15px] text-[#65676b] font-normal leading-normal mb-[20px]'>
+    <div className='mt-[30px]'>
+      <h2 className='text-center text-[32px] font-bold mb-[5px]'>Nhà Cho Thuê</h2>
+      <p className='text-[15px] text-[#65676b] font-normal leading-normal mb-ơ30px]'>
         Kênh thông tin Homestay số 1 Việt Nam - Website cho thuê phòng trọ, nhà nguyên căn, căn hộ, hiệu quả với 100.000+ tin đăng và 2.500.000 lượt xem mỗi tháng.
       </p>
       <PostingList/>
@@ -13,4 +13,4 @@ const RentingRoom = () => {
   )
 }
 
-export default RentingRoom
+export default RentalHouse
